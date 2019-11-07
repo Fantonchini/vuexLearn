b979503fdd330fdeddc267b241d91c423058f179
Vladimir Amatov <vladimir.amatov@softlex.pro>
2019-11-07 11:20:58 +0300

    locale

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div>
+      <a href="#" @click="setLocale('en')">en</a>
+      <a href="#" @click="setLocale('ru')">ru</a>
+    </div>
+    <Languages />
     <PostForm />
     <h1>{{postsCount}}</h1>
     <div class="post" v-for="post in validPosts" :key="post.id">
@@ -12,14 +17,23 @@
 <script>
 import {mapGetters, mapActions} from 'vuex'
 import PostForm from './components/PostForm'
-
+import Languages from './components/languages'
 export default {
   name: 'app',
   components: {
     PostForm,
+    Languages,
   },
   computed: mapGetters(["validPosts","postsCount"]),
-  methods: mapActions(['fetchPosts']),
+  methods: {
+    ...mapActions(['fetchPosts']),
+    setLocale(locale) {
+      import(`./langs/${locale}.json`).then((msgs) => {
+        this.$i18n.setLocaleMessage(locale, msgs)
+        this.$i18n.locale = locale
+      })
+    },
+  },
   async mounted() {
     this.fetchPosts()
   }
---
+++ src/components/languages.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="language">
+        <ul>
+            <li>{{$t('slider.title')}}</li>
+            <li>{{$t('slider.text')}}</li>
+            <li>{{$t('header.nav')}}</li>
+            <li>{{$t('header.link')}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
